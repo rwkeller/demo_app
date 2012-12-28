88dba4fb51d811fbd85ca6e212c939304d5abac1
Rebecca W Keller <rwkeller@realscience4kids.com>
2012-12-28 09:56:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b9577ddfc632cda1e794e2b891f2eca5448d3d197d0598d22f8e707c11d23de69bbc835b390ad59b7bc54aa378c64ab0b3765bdc12e635ac1a385aa5f757abe6'
